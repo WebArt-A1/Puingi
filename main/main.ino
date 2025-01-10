b2f48a82f48b557c62a67f7b7226e58959840540
Art <top49030@gmail.com>
2025-01-10 15:38:19 +1300

    123

--- main/main.ino
+++ main/main.ino
@@ -1,33 +1,32 @@
 #include <ESP8266WiFi.h>
 #include <U8g2lib.h>
+#include <SdFat.h>
 #include <SPI.h>
-#include <SD.h>
+#include <Wire.h>
+#include <Adafruit_MCP23X17.h>
 
-
-// Настройка для SPI интерфейса с дисплеем SH1107
 U8G2_SH1107_PIMORONI_128X128_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 14, /* data=*/ 13, /* cs=*/ 15, /* dc=*/ 2, /* reset=*/ U8X8_PIN_NONE);
 
-File myFile;
+Adafruit_MCP23X17 mcp;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(2000000);
+
+  Wire.begin();
+  mcp.begin_I2C();
+
+  mcp.pinMode(0, OUTPUT);
+  mcp.digitalWrite(0, LOW);
+
   u8g2.begin();
-  u8g2.setFont(u8g2_font_t0_11_t_all);
-  delay(50);
 
-  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_6x13B_tf);
 
-  u8g2.setCursor(0, 9);
-  u8g2.print("Esp load.");
-
-  delay(100);
-
-  u8g2.setCursor(0, 18);
-  u8g2.print("Display load.");
+  u8g2.drawStr(0, 20, "Test");
 
   u8g2.sendBuffer();
 }
 
 void loop() {
-
+  // Основной цикл  
 }
